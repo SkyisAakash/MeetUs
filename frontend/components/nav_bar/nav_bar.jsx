836a54975ad6cbbd28fb3d8f3389fd8eb4048b24
Aakash <sarangaakash@gmail.com>
2018-06-06 15:58:48 -0700

    Finish dropdown menu

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -6,6 +6,8 @@ class NavBar extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {showDropdown: true};
+    this.toggle = this.toggle.bind(this);
   }
   render() {
     return (
@@ -19,18 +21,30 @@ class NavBar extends React.Component {
     );
   }
 
+  toggle() {
+    this.setState({
+      showDropdown: !this.state.showDropdown
+    });
+  }
+
   display() {
   const display = this.props.currentUser ? (
       <div>
         <div id="gear-dropdown-btn">
-  				<i className="fas fa-user-circle"></i>
-          </div>
-            <ul id="gear-dropdown" className="gear-dropdown hidden">
-      			<li><a href="#">Profile</a></li>
-      			<li><a href="#">Settings</a></li>
-      			<li><a href="#" onClick={this.props.logout}>Logout</a></li>
-      		  </ul>
-        <button onClick={this.props.logout}>Logout</button>
+  				<i className="fas fa-user-circle" onClick={this.toggle}></i>
+        </div>
+
+            <ul id="gear-dropdown" className="gear-dropdown" hidden={this.state.showDropdown}>
+              <div className="triangle"></div>
+              <div className="gearbox">
+            <p><a className="gearcontent" href="">Find Groups></a></p>
+              <div className="dropdownlist">
+      			       <li><a className="dropdownOptions" href="#">Profile</a></li>
+      			       <li><a className="dropdownOptions" href="#">Settings</a></li>
+      			       <li><a className="dropdownOptions" id="log" href="#" onClick={this.props.logout}>Logout</a></li>
+            </div>
+            </div>
+          </ul>
       </div>
     ) : (
       <div className="buttons">
