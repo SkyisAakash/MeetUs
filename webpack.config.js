c88c74ac8fa58eb6a04dc19f6a9e9f4908df469b
Aakash <sarangaakash@gmail.com>
2018-06-15 11:43:13 -0700

    change webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,22 @@ var webpack = require("webpack");
 var plugins = []; // if using any plugins for both dev and production
 var devPlugins = []; // if using any plugins for development
 
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
+// var prodPlugins = [
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': JSON.stringify('production')
+//     }
+//   }),
+//   new webpack.optimize.UglifyJsPlugin({
+//     compress: {
+//       warnings: true
+//     }
+//   })
+// ];
 
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
 module.exports = {
   context: __dirname,
   entry: "./frontend/meetus.jsx",
@@ -27,7 +27,25 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
-  plugins: plugins,
+  // process: {env: {
+  //   'NODE_ENV': 'production'
+  // }},
+  // optimization: {
+     // minimizer: [
+       // new webpack.optimize.minimize({
+       //   uglifyOptions: {
+       //     // beautify: false,
+       //     compress: true,
+       //     // comments: false,
+       //     // mangle: false,
+       //     // toplevel: false,
+       //     // keep_classnames: true, // <-- doesn't exist, I guess. It's in harmony branch
+       //     // keep_fnames: true //
+       //   }
+       // })
+     // ]
+   // },
+   // mode: 'production',
   module: {
     rules: [
       {
